8005f4ead0171b412abe3e336a357eeddfa28b86
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-08-29 11:21:50 +0530

    fix code formatting

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $assetVersion = asset_version();
             <img src="assets/images/examify_logo.png" alt="." class="footer-logo">
             <span>Examify</span>
         </a>
-        <p class="footer-copy">&copy; <?php echo date('Y'); ?> Examify. All rights reserved.</p>
+        <p class="footer-copy">&copy; <?= date('Y') ?> Examify. All rights reserved.</p>
     </footer>
 
 </body>
